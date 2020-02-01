55038ebb37ac3fde3c2718e05a1a40b5306ebaca
Abbabon <netanel.amit@gmail.com>
2020-02-01 16:28:07 +0200

    scene order

---
+++ Assets/Scripts/Interactors/Cereal/RotatableCereal.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Zenject;
+
+public class RotatableCereal : Rotatable
+{
+    [Inject] private SceneManagerService _sceneManagerService;
+    [Inject] private IPromiseTimerService _promiseTimerService;
+    
+    private bool _locked = false;
+    
+    protected override void OnRotateTarget(float rotationAngle)
+    {
+        _transform.localRotation = Quaternion.Euler(0, 0, rotationAngle);
+        ChangeInputState(false);
+        
+        if (!_locked)
+        {
+            _locked = true;
+            _promiseTimerService.WaitFor(1f).Then(() => _sceneManagerService.UnloadSceneAndLoadNext(ScenesEnum.CerealLevel3));
+        }
+    }
+}
